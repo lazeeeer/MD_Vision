087f63e05b3adce639f9bafd001dd326ebbdc766
lazeeeer <krsticlazar2002@outlook.com>
2024-11-17 01:55:48 -0500

    changed main for c++ compatibility, added in rf_comms which uses RadioLib libarary in c++

---
+++ components/rf_comms/rf_comms.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <RadioLib.h>
+
+#include "rf_comms.h"
+
+using namespace std;
+
+// ==== Testing Shid =====================================
+
+
+// RF69 radio = new Module(10, 2, 9, 3);
+// PagerClient pager(&radio);
+
+void initRadio()
+{
+    cout << "This will init the radio:\n";
+}
